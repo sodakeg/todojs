a68114202288b3400226faccd49d62f3e4517435
Souyama Debnath <souyama.debnath@atos.net>
2022-04-07 18:21:23 +0530

    eject react-scripts

---
+++ config/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: false,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+  },
+};
