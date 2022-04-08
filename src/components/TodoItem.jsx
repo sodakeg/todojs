c537b52c882165b7e71cbf4dbe3a96eab07b59f1
Souyama Debnath <souyama.debnath@atos.net>
2022-04-08 09:15:36 +0530

    Create todo item component

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Accordion, AccordionActions, AccordionDetails, AccordionSummary, Box, Grid, Typography,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+import EditButton from './EditButton';
+import DeleteButton from './DeleteButton';
+
+function TodoItem({
+  id, title, description, done, updatedAt,
+}) {
+  const [expanded, setExpanded] = React.useState(false);
+  return (
+    <Accordion
+      expanded={expanded}
+      sx={{
+        width: '100%', alignSelf: 'center', ':hover': { boxShadow: 3 },
+      }}
+      onChange={() => setExpanded(!expanded)}
+    >
+      {/* strikeout entries marked as done */}
+
+      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+        <Grid container direction="row" justifyContent="space-around" alignItems="baseline">
+          <Grid item xs="3">
+            <Box sx={{ textDecoration: done ? 'line-through' : 'none' }}>
+              <Typography variant="h5" component="div" width="90%" flexShrink={0}>
+                {title}
+              </Typography>
+            </Box>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Typography color="text.secondary" sx={{ textAlign: 'right' }} gutterBottom>
+              {updatedAt}
+            </Typography>
+          </Grid>
+
+        </Grid>
+      </AccordionSummary>
+
+      <AccordionDetails>
+        <Box sx={{ textDecoration: done ? 'line-through' : 'none' }}>
+          <Typography variant="body2" component="p">
+            {description}
+          </Typography>
+        </Box>
+      </AccordionDetails>
+
+      <AccordionActions>
+        <EditButton id={id} />
+        <DeleteButton id={id} />
+      </AccordionActions>
+    </Accordion>
+  );
+}
+
+TodoItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  done: PropTypes.bool.isRequired,
+  updatedAt: PropTypes.string.isRequired,
+};
+
+export default TodoItem;
