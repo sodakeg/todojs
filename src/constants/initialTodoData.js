0d9656b2ace3345664203fdf8b55b6cad9311d4e
Souyama Debnath <souyama.debnath@atos.net>
2022-04-08 09:10:21 +0530

    Create file: `initialTodoData.js`

---
+++ src/constants/initialTodoData.js
@@ -0,0 +1,46 @@
+const initialTodoData = [
+  {
+    id: 1,
+    title: 'Hello',
+    description: 'Welcome to sample todo test',
+    done: false,
+    updatedAt: window.Date.now(),
+  },
+  {
+    id: 2,
+    title: 'You may mark messages as done',
+    description: 'By clicking the checkbox',
+    done: false,
+    updatedAt: window.Date.now(),
+  },
+  {
+    id: 3,
+    title: 'You may remove messages',
+    description: 'By clicking the trash con',
+    done: false,
+    updatedAt: window.Date.now(),
+  },
+  {
+    id: 4,
+    title: 'You may edit messages',
+    description: 'By clicking the pencil icon',
+    done: false,
+    updatedAt: window.Date.now(),
+  },
+  {
+    id: 5,
+    title: 'You may add messages',
+    description: 'By clicking the plus icon',
+    done: false,
+    updatedAt: window.Date.now(),
+  },
+  {
+    id: 6,
+    title: 'You may clear all messages',
+    description: 'By clicking the clear icon',
+    done: false,
+    updatedAt: window.Date.now(),
+  },
+];
+
+export default initialTodoData;
